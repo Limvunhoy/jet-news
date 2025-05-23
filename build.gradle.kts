cc8471cd23f4bb900641c25a0a531ab8407cd711
vunhoy.lim <vunhoy.lim@wingbank.com.kh>
2025-05-23 13:56:35 +0700

    config sonarqube

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,14 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("org.sonarqube") version "3.2.0"
+    id("org.sonarqube") version "6.0.1.5171"
 
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
 }
 
-//sonarqube {
-//    properties {
-//        property("sonar.projectKey", "your-project-key")
-//        property("sonar.host.url", "http://localhost:9000")
-//        property("sonar.login", "your-authentication-token")
-//    }
-//}
+sonar {
+    properties {
+        property("sonar.projectKey", "android-kotlin")
+        property("sonar.projectName", "android-kotlin")
+    }
+}
