c95b9b7babe414f55ae187fd0885b142bbd11dbe
vunhoy.lim <vunhoy.lim@wingbank.com.kh>
2025-05-26 10:25:51 +0700

    update ci/cd script for pull request job

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,6 @@ plugins {
 sonar {
     properties {
         property("sonar.projectKey", "JetNews")
-        property("sonar.host.url", "http://202.79.29.108:9000")
         property("sonar.projectName", "JetNews")
     }
 }
